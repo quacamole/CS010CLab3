3f84575e56082bf034d052211a96b95f866c8748
quacamole <102833311+quacamole@users.noreply.github.com>
2022-04-14 10:07:20 -0700

    added try/catch

--- main.cpp
+++ main.cpp
@@ -15,22 +15,28 @@ int main(){
     char curChar;
     int index;
     int numOfRuns = 10;
-    while(--numOfRuns >= 0){
+    while(--numOfRuns >= 0) {
         cout << "Enter a number: " << endl;
         cin >> index;
-        curChar = getElement(vals,index);
-        cout << "Element located at " << index << ": is " << curChar << endl;
+        try {
+            curChar = getElement(vals, index);
+            cout << "Element located at " << index << ": is " << curChar << endl;
+        }
+        catch (out_of_range &err) {
+            cout << "Out of range exception occurred." << endl;
+        }
+        return 0;
     }
-    return 0;
 }
-vector<char> createVector(){
+
+vector<char> createVector() {
     int vecSize = rand() % 26;
     char c = 'a';
     vector<char> vals;
-    for(int i = 0; i < vecSize; i++)
-    {
+    for (int i = 0; i < vecSize; i++) {
         vals.push_back(c);
         c++;
     }
     return vals;
+
 }
