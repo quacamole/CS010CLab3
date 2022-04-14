68e094ce5a75f1055d69dbee44f00c4f1326bdae
btruj007 <102835844+btruj007@users.noreply.github.com>
2022-04-14 10:15:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,29 +1,52 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <stdexcept>
 #include <cstdlib>
-
+#include <vector>
+#include <string>
 using namespace std;
 
-
-int main(){
-
-    //Part B
-    srand(time(0));
-    vector<char> vals = createVector();
-    char curChar;
-    int index;
-    int numOfRuns = 10;
-    while(--numOfRuns >= 0){
-        cout << "Enter a number: " << endl;
-        cin >> index;
-        curChar = getElement(vals,index);
-        cout << "Element located at " << index << ": is " << curChar << endl;
+template<typename T>
+unsigned min_index(const vector<T> &vals, unsigned index) {
+    unsigned int min = index;
+    
+    if(vals.size() > 0)
+    {
+        for (unsigned int i = index; i < vals.size(); i++) {
+            if (vals.at(i) < vals.at(min)) {
+                min = i;
+            }
+        }
+        return min;
     }
-    return 0;
+    
+    return -1;
 }
-vector<char> createVector(){
+
+template<typename T>
+void selection_sort(vector<T> &vals) {
+    if (vals.size() > 0)
+    {
+        for (unsigned i = 0; i < vals.size(); i++)
+        {
+            unsigned minIndex = min_index(vals, i);
+            T tempVal = vals.at(minIndex);
+
+            vals.at(minIndex) = vals.at(i);
+            vals.at(i) = tempVal;
+        }
+    }
+}
+
+template<typename T>
+T getElement(vector<T> vals, int index) {
+    if(index < 0 || index > vals.size() - 1)
+    {
+        throw runtime_error("std::out_of_range");
+    }
+    return vals.at(index);
+}
+
+vector<char> createVector() {
     int vecSize = rand() % 26;
     char c = 'a';
     vector<char> vals;
@@ -33,4 +56,25 @@ vector<char> createVector(){
         c++;
     }
     return vals;
+}
+
+int main(){
+    //Part B
+    srand(time(0));
+    vector<char> vals = createVector();
+    char curChar;
+    int index;
+    int numOfRuns = 10;
+    while(--numOfRuns >= 0){
+        cout << "Enter a number: " << endl;
+        cin >> index;
+        try {
+        curChar = getElement(vals,index);
+        cout << "Element located at " << index << ": is " << curChar << endl;
+        }
+    }
+    catch (runtime_error& excpt) {
+        cout << excpt.what() << " exception occurred." << endl;
+    }
+    return 0;
 }
